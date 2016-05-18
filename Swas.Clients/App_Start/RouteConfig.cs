54799c9a9045165e2cfc999de79293dc08d0fad4
alpha <mania.anri@gmail.com>
2016-05-18 16:27:00 +0400

    a

--- Swas.Clients/App_Start/RouteConfig.cs
+++ Swas.Clients/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Swas.Clients
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "SolidWasteAct", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
